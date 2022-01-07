cbcb53b1dd95863426624fd60dea1f6c0a9f3254
m00racle <yanuar.prakosa@gmail.com>
2022-01-07 09:26:56 +0700

    fix: tap go key hide keyboard but need some waiting time

--- ClimaUITests/ClimaKeyboardInteractionTests.swift
+++ ClimaUITests/ClimaKeyboardInteractionTests.swift
@@ -80,6 +80,6 @@ class ClimaKeyboardInteractionTests: XCTestCase {
         app/*@START_MENU_TOKEN@*/.buttons["Go"]/*[[".keyboards",".buttons[\"go\"]",".buttons[\"Go\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
         
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTAssertFalse(app.keys.element(boundBy: 0).exists)
+        XCTAssertFalse(app.keyboards.buttons["a"].waitForExistence(timeout: 5))
     }
 }
